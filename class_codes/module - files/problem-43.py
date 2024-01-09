0e573deb01ca71ec2780255b67cefa9b7ee53135
kafka <sajon.tahsen@gmail.com>
2024-01-09 22:45:59 +0600

    added code for file modules

---
+++ class_codes/module - files/problem-43.py
@@ -0,0 +1,7 @@
+# Dictionary of Squares: Use a loop to create a dictionary where keys are numbers and values are their squares.
+
+result = {}
+for i in range(1, 6):
+    result[i] = i * i
+
+print(result)
